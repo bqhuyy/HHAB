aed0af32127376fda886ffdde5ce719de0eb0bad
bqhuy <bqhuy@apcs.vn>
2019-08-10 22:41:21 +0700

    add ui main menu

---
+++ Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneFader fader;
+
+    private string multiplayerScene = "MultiplayerScene";
+    private string inGameScene = "InGameScene";
+
+    public void Play()
+    {
+        fader.FadeTo(inGameScene);
+    }
+
+    public void Multplayer()
+    {
+        fader.FadeTo(multiplayerScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
