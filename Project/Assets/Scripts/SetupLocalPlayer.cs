3a1c91765f9eac5027a3d21c2fccc8df4e39c3b3
An Pham <phvietan@gmail.com>
2019-08-15 11:01:07 +0700

    add some 3rd person ingame multiplayer

---
+++ Project/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<ThirdPersonCharacter>().enabled = true;
+            GetComponent<ThirdPersonUserControl>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
