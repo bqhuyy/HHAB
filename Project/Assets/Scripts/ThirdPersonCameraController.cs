105ea01aee37e6954721fb51b61292480be96743
DinhHieuHoang <hdhieu@apcs.vn>
2019-08-17 22:19:23 +0700

    tps using mouse

---
+++ Project/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensivityX = 4.0f;
+    private float sensivityY = 1.0f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+}
