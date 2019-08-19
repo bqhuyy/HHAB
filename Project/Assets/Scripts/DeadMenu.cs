80d45d6bd951512dffac44df40332a3bf3d3cbc9
bqhuy <bqhuy@apcs.vn>
2019-08-19 23:51:33 +0700

    add dead menu

---
+++ Project/Assets/Scripts/DeadMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadMenu : MonoBehaviour
+{
+    public SceneFader fader;
+    const string multiplayerScene = "MultiplayerScene";
+    // Start is called before the first frame update
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        fader.FadeTo(multiplayerScene);
+    }
+}
