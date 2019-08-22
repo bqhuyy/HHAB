3727761e7cb2384ffac273f89877a3f0477ca688
An Pham <phvietan@gmail.com>
2019-08-23 00:25:13 +0700

    done singleplayer

---
+++ Project/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControlBackup.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class AICharacterControl : MonoBehaviour
+    {
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;                                    // target to aim for
+
+        private void Start()
+        {
+            character = GetComponent<ThirdPersonCharacter>();
+        }
+
+
+        private void Update()
+        {
+            FindClosestTarget();
+
+            if (target == null) return;
+
+            character.Move(target.position - transform.position, false, false);
+        }
+
+        private void FixedUpdate()
+        {
+           // FindClosestTarget();
+        }
+
+        private void FindClosestTarget()
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            if (players.Length > 0)
+            {
+                float minDistance = Vector3.Distance(players[0].transform.position, gameObject.transform.position);
+                Transform currentTarget = players[0].transform;
+                foreach (GameObject player in players)
+                {
+                    float tmp = Vector3.Distance(player.transform.position, gameObject.transform.position);
+                    if (tmp < minDistance)
+                    {
+                        minDistance = tmp;
+                        currentTarget = player.transform;
+                    }
+                }
+                this.SetTarget(currentTarget);
+            }
+        }
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
