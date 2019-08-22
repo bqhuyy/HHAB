37e6c00d0951bd02e2389d579ec49852705621d3
An Pham <phvietan@gmail.com>
2019-08-22 20:13:37 +0700

    add scene single player and remove some unused scene

--- Project/Assets/Scripts/ZombieSpawnSinglePlayer.cs
+++ Project/Assets/Scripts/ZombieSpawnSinglePlayer.cs
@@ -5,32 +5,18 @@ using UnityEngine;
 public class ZombieSpawnSingleplayer : MonoBehaviour
 {
     public GameObject zombiePrefab;
-    public float radius = 10;
-    public int numberZombie = 10;
-    public int timeBetweenSpawn = 10; // 10 sec spawn numberZombie zombies
+    public int timeBetweenSpawn = 5; 
 
-    private float lastSpawnTime = -100000;
-
-    // Update is called once per frame
     void Start()
     {
         StartCoroutine("SpawnZombie");
     }
+
     IEnumerator SpawnZombie()
     {
         for (; ; )
         {
-            // Spawn numberZombie times
-            for (int i = 0; i < numberZombie; ++i)
-            {
-                int xPos = Random.Range((int)-radius, (int)radius);
-                int maxZ = (int)Mathf.Sqrt(radius * radius - xPos * xPos);
-                int zPos = Random.Range(-maxZ, maxZ);
-
-                Vector3 spawnLocation = transform.position + new Vector3(xPos, 0, zPos);
-                Instantiate(zombiePrefab, spawnLocation, Quaternion.identity);
-            }
-            
+            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(timeBetweenSpawn);
         }
     }
